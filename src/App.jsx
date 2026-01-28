4a5d3e7fb51d111f0007525d6f5ce3a53994944c
Altaf Raja <altaf.1040216@gmail.com>
2026-01-28 23:03:41 +0530

    Add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
-    <>
-      <h1>App component</h1>
-    </>
+    <div className="max-w-350 mx-auto">
+      <Navbar />
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+function Navbar() {
+      return (
+            <div className="flex justify-between items-center p-5 border-gray-200 border-b">
+                  <h1 className="text-3xl font-bold text-gray-800">PixelPipe</h1>
+                  <h5 className="text-gray-500">Made with ❤️ by Altaf Raja</h5>
+            </div>
+      )
+}
+
+export default Navbar
