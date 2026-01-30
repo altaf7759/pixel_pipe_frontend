83186783908721fbb995796fbd93fbaff466df02
Altaf Raja <altaf.1040216@gmail.com>
2026-01-30 18:53:31 +0530

    Navbar fix for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,12 @@
 function Navbar() {
       return (
             <div className="flex justify-between items-center p-5 border-gray-200 border-b">
-                  <h1 className="text-3xl font-bold text-gray-800">PixelPipe</h1>
-                  <h5 className="text-gray-500">Made with ❤️ by Altaf Raja</h5>
+                  <h1 className="text-2xl md:text-3xl font-bold text-gray-800">PixelPipe</h1>
+                  <h5 className="text-gray-500 text-sm md:text-base text-center">
+                        Made with ❤️
+                        <br className="block md:hidden" />
+                        by Altaf Raja
+                  </h5>
             </div>
       )
 }
